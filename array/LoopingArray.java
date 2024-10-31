f78e97aa3ad992af774806ea043c9f406fff491e
Hafid Fathurrohman <137675174+ace1089@users.noreply.github.com>
2024-10-31 12:16:49 +0700

    Update LoopingArray.java

--- array/LoopingArray.java
+++ array/LoopingArray.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  *
  * @author Hafid Fathurrohman
  */
-public class BELAJAR {
+public class LoopingArray {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
